145972dbb9abcd61f0d61b480cf533a0e42e1b6f
Balaji Narain <narainbalaji@gmail.com>
2014-05-07 17:33:42 +0530

    BalajiN | Adding a vagrant evironment to which you can install bahmni

---
+++ scripts/provision-local.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+yum install -y http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum install -y https://github.com/SharedHealth/FreeSHR-SCM/raw/master/dist/shr_scm_utils-0.1-1.noarch.rpm
+yum install -y https://github.com/SharedHealth/FreeSHR-SCM/raw/master/dist/shr_scm-0.1-1.noarch.rpm
+mv -f /vagrant/vagrant-config/hosts /etc/ansible/hosts
