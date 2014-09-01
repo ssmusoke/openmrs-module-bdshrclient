af3b4997eb34b79f5f41f75dece23d306200164c
Balaji Narain <balajin@thoughtworks.com>
2014-09-01 16:11:06 +0530

    BalajiN | Fixing the download part

--- shrclient-omod/src/main/java/org/openmrs/module/shrclient/web/controller/dto/ResourceOrFeedDeserializer.java
+++ shrclient-omod/src/main/java/org/openmrs/module/shrclient/web/controller/dto/ResourceOrFeedDeserializer.java
@@ -3,6 +3,7 @@ package org.openmrs.module.shrclient.web.controller.dto;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.node.TextNode;
 import org.hl7.fhir.instance.formats.ParserBase;
 import org.hl7.fhir.instance.formats.XmlParser;
 
@@ -13,7 +14,7 @@ public class ResourceOrFeedDeserializer extends JsonDeserializer<ParserBase.Reso
     @Override
     public ParserBase.ResourceOrFeed deserialize(JsonParser jp, DeserializationContext ctx) {
         try {
-            final String xml = jp.readValueAsTree().toString();
+            final String xml = ((TextNode) jp.readValueAsTree()).textValue();
             return new XmlParser(true).parseGeneral(new ByteArrayInputStream(xml.getBytes()));
         } catch (Exception e) {
             throw new RuntimeException(e);
