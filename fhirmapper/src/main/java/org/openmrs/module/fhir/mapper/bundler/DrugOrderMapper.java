ac9ed8884b5a86e76edfa222eaa80374f4348c94
Nehashri <nehashrl@thoughtworks.com>
2015-09-10 18:45:14 +0530

    Neha | drug order mapping temp.

--- fhirmapper/src/main/java/org/openmrs/module/fhir/mapper/bundler/DrugOrderMapper.java
+++ fhirmapper/src/main/java/org/openmrs/module/fhir/mapper/bundler/DrugOrderMapper.java
@@ -1,17 +1,32 @@
 package org.openmrs.module.fhir.mapper.bundler;
 
+import ca.uhn.fhir.model.api.IDatatype;
+import ca.uhn.fhir.model.api.TemporalPrecisionEnum;
+import ca.uhn.fhir.model.dstu2.composite.IdentifierDt;
+import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
+import ca.uhn.fhir.model.dstu2.composite.QuantityDt;
+import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
+import ca.uhn.fhir.model.dstu2.composite.TimingDt;
 import ca.uhn.fhir.model.dstu2.resource.Bundle;
 import ca.uhn.fhir.model.dstu2.resource.Encounter;
+import ca.uhn.fhir.model.dstu2.resource.MedicationPrescription;
+import ca.uhn.fhir.model.primitive.DecimalDt;
+import org.apache.commons.collections.CollectionUtils;
 import org.openmrs.DrugOrder;
 import org.openmrs.Order;
+import org.openmrs.Provider;
+import org.openmrs.module.fhir.mapper.model.EntityReference;
 import org.openmrs.module.fhir.utils.CodableConceptService;
+import org.openmrs.module.fhir.utils.PropertyKeyConstants;
 import org.openmrs.module.fhir.utils.UnitsHelpers;
 import org.openmrs.module.shrclient.dao.IdMappingsRepository;
+import org.openmrs.module.shrclient.model.IdMapping;
 import org.openmrs.module.shrclient.util.SystemProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.openmrs.module.fhir.mapper.MRSProperties.MRS_DRUG_ORDER_TYPE;
@@ -34,63 +49,93 @@ public class DrugOrderMapper implements EmrOrderResourceHandler {
 
     @Override
     public List<FHIRResource> map(Order order, Encounter fhirEncounter, Bundle bundle, SystemProperties systemProperties) {
-//        ArrayList<FHIRResource> FHIRResources = new ArrayList<>();
-//        DrugOrder drugOrder = (DrugOrder) order;
-//        MedicationPrescription prescription = new MedicationPrescription();
-//        prescription.setEncounter(fhirEncounter.getIndication());
-//        setPatient(fhirEncounter, prescription);
-//        prescription.setDateWritten(getDateWritten(drugOrder));
-//        prescription.setMedication(getMedication(drugOrder));
-//        prescription.setPrescriber(getParticipant(drugOrder, fhirEncounter, systemProperties));
-//        setDoseInstructions(drugOrder, prescription, systemProperties);
-//        Identifier identifier = prescription.addIdentifier();
-//        identifier.setValueSimple(new EntityReference().build(Order.class, systemProperties, order.getUuid()));
-//        FHIRResources.add(new FHIRResource("Medication Prescription", prescription.getIdentifier(), prescription));
-//        return FHIRResources;
-        return Collections.emptyList();
+        List<FHIRResource> FHIRResources = new ArrayList<>();
+        DrugOrder drugOrder = (DrugOrder) order;
+        MedicationPrescription prescription = new MedicationPrescription();
+        prescription.setEncounter(new ResourceReferenceDt().setReference(fhirEncounter.getId().getValueAsString()));
+        setPatient(fhirEncounter, prescription);
+        prescription.setDateWritten(drugOrder.getDateCreated(), TemporalPrecisionEnum.MILLI);
+        prescription.setMedication(getMedication(drugOrder));
+        prescription.setPrescriber(getOrdererReference(drugOrder, fhirEncounter, systemProperties));
+        setDoseInstructions(drugOrder, prescription, systemProperties);
+        IdentifierDt identifier = prescription.addIdentifier();
+        identifier.setValue(new EntityReference().build(Order.class, systemProperties, order.getUuid()));
+        FHIRResources.add(new FHIRResource("Medication Prescription", prescription.getIdentifier(), prescription));
+        return FHIRResources;
+        //org.openmrs.module.bahmniemrapi.drugorder.dosinginstructions.FlexibleDosingInstructions
     }
 
-//    protected ResourceReference getParticipant(DrugOrder order, Encounter encounter, SystemProperties systemProperties) {
-//        if (order.getOrderer() != null) {
-//            String providerUrl = new EntityReference().build(Provider.class, systemProperties, order.getOrderer().getIdentifier());
-//            if (providerUrl != null) {
-//                return new ResourceReference().setReferenceSimple(providerUrl);
-//            }
-//        }
-//        List<Encounter.EncounterParticipantComponent> participants = encounter.getParticipant();
-//        if (!CollectionUtils.isEmpty(participants)) {
-//            return participants.get(0).getIndividual();
-//        }
-//        return null;
-//    }
-//
-//    private void setDoseInstructions(DrugOrder drugOrder, MedicationPrescription prescription, SystemProperties systemProperties) {
-//        MedicationPrescription.MedicationPrescriptionDosageInstructionComponent dosageInstruction = prescription.addDosageInstruction();
-//        if (null != drugOrder.getRoute()) {
-//            dosageInstruction.setRoute(
-//                    codableConceptService.getTRValueSetCodeableConcept(drugOrder.getRoute(),
-//                            systemProperties.getTrValuesetUrl(PropertyKeyConstants.TR_VALUESET_ROUTE)));
-//        }
-//        setDoseQuantity(drugOrder, dosageInstruction, systemProperties);
-//        dosageInstruction.setTiming(getSchedule(drugOrder));
-//    }
-//
-//
+    private ResourceReferenceDt getOrdererReference(Order order, Encounter encounter, SystemProperties systemProperties) {
+        if (order.getOrderer() != null) {
+            String providerUrl = new EntityReference().build(Provider.class, systemProperties, order.getOrderer().getIdentifier());
+            if (providerUrl != null) {
+                return new ResourceReferenceDt().setReference(providerUrl);
+            }
+        }
+        List<Encounter.Participant> participants = encounter.getParticipant();
+        if (!CollectionUtils.isEmpty(participants)) {
+            return participants.get(0).getIndividual();
+        }
+        return null;
+    }
+
+    private void setDoseInstructions(DrugOrder drugOrder, MedicationPrescription prescription, SystemProperties systemProperties) {
+        MedicationPrescription.DosageInstruction dosageInstruction = prescription.addDosageInstruction();
+        if (null != drugOrder.getRoute()) {
+            dosageInstruction.setRoute(
+                    codableConceptService.getTRValueSetCodeableConcept(drugOrder.getRoute(),
+                            systemProperties.getTrValuesetUrl(PropertyKeyConstants.TR_VALUESET_ROUTE)));
+        }
+        setDoseQuantity(drugOrder, dosageInstruction, systemProperties);
+        dosageInstruction.setScheduled(getSchedule(drugOrder));
+    }
+
+    private IDatatype getSchedule(DrugOrder drugOrder) {
+        TimingDt timing = new TimingDt();
+        TimingDt.Repeat repeat = new TimingDt.Repeat();
+
+        setEvent(drugOrder, repeat);
+        setDuration(drugOrder, repeat);
+        setFrequencyAndPeriod(drugOrder, repeat);
+
+        timing.setRepeat(repeat);
+        return timing;
+    }
+
+    private void setFrequencyAndPeriod(DrugOrder drugOrder, TimingDt.Repeat repeat) {
+        String frequencyConceptName = drugOrder.getFrequency().getConcept().getName().getName();
+        UnitsHelpers.FrequencyUnits frequencyUnit = unitsHelpers.getFrequencyUnits(frequencyConceptName);
+        repeat.setFrequency(frequencyUnit.getFrequency());
+        repeat.setPeriod(frequencyUnit.getFrequencyPeriod());
+        repeat.setPeriodUnits(frequencyUnit.getUnitOfTime());
+    }
+
+    private void setDuration(DrugOrder drugOrder, TimingDt.Repeat repeat) {
+        repeat.setDuration(drugOrder.getDuration());
+        if (drugOrder.getDurationUnits() != null) {
+            String units = drugOrder.getDurationUnits().getName().getName();
+            UnitsHelpers.UnitToDaysConverter unitToDaysConverter = unitsHelpers.getDurationUnitToUnitMapper().get(units);
+            repeat.setDurationUnits(unitToDaysConverter.getUnitsOfTime());
+        }
+    }
+
+
 //    private Schedule getSchedule(DrugOrder drugOrder) {
 //        Schedule schedule = new Schedule();
 //        setEvent(drugOrder, schedule);
 //        setRepeatComponent(drugOrder, schedule);
 //        return schedule;
 //    }
-//
-//    private void setEvent(DrugOrder drugOrder, Schedule schedule) {
-//        java.util.Date scheduledDate = drugOrder.getScheduledDate();
-//        if (null != scheduledDate) {
-//            Period period = schedule.addEvent();
-//            period.setStartSimple(new DateAndTime(scheduledDate));
-//        }
-//    }
-//
+
+    private void setEvent(DrugOrder drugOrder, TimingDt.Repeat repeat) {
+        java.util.Date scheduledDate = drugOrder.getScheduledDate();
+        if (null != scheduledDate) {
+            PeriodDt period = new PeriodDt();
+            period.setStart(scheduledDate, TemporalPrecisionEnum.MILLI);
+            repeat.setBounds(period);
+        }
+    }
+
 //    private void setRepeatComponent(DrugOrder drugOrder, Schedule schedule) {
 //        if (null != drugOrder.getFrequency() && null != drugOrder.getDuration()) {
 //            Decimal duration = getDuration();
@@ -103,13 +148,13 @@ public class DrugOrderMapper implements EmrOrderResourceHandler {
 //            schedule.setRepeat(repeatComponent);
 //        }
 //    }
-//
+
 //    private Decimal getDuration() {
 //        Decimal duration = new Decimal();
 //        duration.setValue(new BigDecimal(DEFAULT_DURATION));
 //        return duration;
 //    }
-//
+
 //    private void setCount(DrugOrder drugOrder, UnitsHelpers unitsHelpers, Schedule schedule, Schedule.UnitsOfTime frequencyUnit, ScheduleRepeatComponent repeatComponent) {
 //        double unitsToDaysMultiplier = getUnitsToDaysMultiplier(drugOrder, unitsHelpers, frequencyUnit);
 //        Integer integer = new Integer();
@@ -117,7 +162,7 @@ public class DrugOrderMapper implements EmrOrderResourceHandler {
 //        int countValue = (int) Math.round(drugOrder.getDuration().intValue() * unitsToDaysMultiplier);
 //        integer.setValue(countValue);
 //    }
-//
+
 //    private double getUnitsToDaysMultiplier(DrugOrder drugOrder, UnitsHelpers unitsHelpers, Schedule.UnitsOfTime frequnecyUnit) {
 //        UnitsHelpers.UnitToDaysConverter unitToDaysConverter = unitsHelpers.getDurationUnitToUnitMapper().get(drugOrder.getDurationUnits().getName().getName());
 //        double unitsToDaysMultiplier = 1;
@@ -126,46 +171,39 @@ public class DrugOrderMapper implements EmrOrderResourceHandler {
 //        }
 //        return unitsToDaysMultiplier;
 //    }
-//
+
 //    private int getFrequencyPerUnit(DrugOrder drugOrder, Schedule.UnitsOfTime frequencyUnit, UnitsHelpers unitsHelpers) {
 //        double frequencyPerUnit = (drugOrder.getFrequency().getFrequencyPerDay()) * unitsHelpers.getUnitsOfTimeMapper().get(frequencyUnit).getInDays();
 //        return (int) Math.round(frequencyPerUnit);
 //    }
-//
-//
-//    private void setDoseQuantity(DrugOrder drugOrder, MedicationPrescription.MedicationPrescriptionDosageInstructionComponent dosageInstruction, SystemProperties systemProperties) {
-//        if (null != drugOrder.getDose()) {
-//            Quantity doseQuantity = new Quantity();
-//            Decimal dose = new Decimal();
-//            dose.setValue(new BigDecimal(drugOrder.getDose()));
-//            if(null != drugOrder.getDoseUnits()) {
-//                if (null != idMappingsRepository.findByInternalId(drugOrder.getDoseUnits().getUuid())) {
-//                    String code = codableConceptService.getTRValueSetCode(drugOrder.getDoseUnits());
-//                    doseQuantity.setCodeSimple(code);
-//                    doseQuantity.setSystemSimple(systemProperties.getTrValuesetUrl(PropertyKeyConstants.TR_VALUESET_QUANTITY_UNITS));
-//                }
-//            }
-//            dosageInstruction.setDoseQuantity(doseQuantity.setValue(dose));
-//        }
-//    }
-//
-//    private ResourceReference getMedication(DrugOrder drugOrder) {
-//        ResourceReference resourceReference = new ResourceReference();
-//        String uuid = drugOrder.getDrug().getUuid();
-//        IdMapping idMapping = idMappingsRepository.findByInternalId(uuid);
-//        if (null != idMapping)
-//            resourceReference.setReferenceSimple(idMapping.getUri());
-//        resourceReference.setDisplaySimple(drugOrder.getDrug().getDisplayName());
-//        return resourceReference;
-//    }
-//
-//    private DateTime getDateWritten(DrugOrder drugOrder) {
-//        DateTime dateTime = new DateTime();
-//        dateTime.setValue(new DateAndTime(drugOrder.getDateCreated()));
-//        return dateTime;
-//    }
-//
-//    private void setPatient(Encounter fhirEncounter, MedicationPrescription medicationPrescription) {
-//        medicationPrescription.setPatient(fhirEncounter.getSubject());
-//    }
+
+    private void setDoseQuantity(DrugOrder drugOrder, MedicationPrescription.DosageInstruction dosageInstruction, SystemProperties systemProperties) {
+        if (null != drugOrder.getDose()) {
+            QuantityDt doseQuantity = new QuantityDt();
+            DecimalDt dose = new DecimalDt();
+            dose.setValue(new BigDecimal(drugOrder.getDose()));
+            if (null != drugOrder.getDoseUnits()) {
+                if (null != idMappingsRepository.findByInternalId(drugOrder.getDoseUnits().getUuid())) {
+                    String code = codableConceptService.getTRValueSetCode(drugOrder.getDoseUnits());
+                    doseQuantity.setCode(code);
+                    doseQuantity.setSystem(systemProperties.getTrValuesetUrl(PropertyKeyConstants.TR_VALUESET_QUANTITY_UNITS));
+                }
+            }
+            dosageInstruction.setDose(doseQuantity.setValue(dose));
+        }
+    }
+
+    private ResourceReferenceDt getMedication(DrugOrder drugOrder) {
+        ResourceReferenceDt resourceReference = new ResourceReferenceDt();
+        String uuid = drugOrder.getDrug().getUuid();
+        IdMapping idMapping = idMappingsRepository.findByInternalId(uuid);
+        if (null != idMapping)
+            resourceReference.setReference(idMapping.getUri());
+        resourceReference.setDisplay(drugOrder.getDrug().getDisplayName());
+        return resourceReference;
+    }
+
+    private void setPatient(Encounter fhirEncounter, MedicationPrescription medicationPrescription) {
+        medicationPrescription.setPatient(fhirEncounter.getPatient());
+    }
 }
--- fhirmapper/src/main/java/org/openmrs/module/fhir/utils/UnitsHelpers.java
+++ fhirmapper/src/main/java/org/openmrs/module/fhir/utils/UnitsHelpers.java
@@ -1,97 +1,145 @@
 package org.openmrs.module.fhir.utils;
 
+import ca.uhn.fhir.model.dstu2.valueset.UnitsOfTimeEnum;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static ca.uhn.fhir.model.dstu2.valueset.UnitsOfTimeEnum.*;
 
 
 @Component
 public class UnitsHelpers {
-    //TODO : rename
     public enum UnitToDaysConverter {
-//        Hour("Hour(s)", h, 0.041667),
-//        Day("Day(s)", d, 1),
-//        Week("Week(s)", wk, 7),
-//        Month("Month(s)", mo, 30);
-//
-//        private final String units;
-//        private final UnitsOfTime unitsOfTime;
-//        private final double inDays;
-//
-//        UnitToDaysConverter(String units, UnitsOfTime unitsOfTime, double inDays) {
-//            this.units = units;
-//            this.unitsOfTime = unitsOfTime;
-//            this.inDays = inDays;
-//        }
-//
-//        public String getUnits() {
-//            return units;
-//        }
-//
-//        public UnitsOfTime getUnitsOfTime() {
-//            return unitsOfTime;
-//        }
-//
-//        public double getInDays() {
-//            return inDays;
-//        }
+        Hour("Hour(s)", H),
+        Day("Day(s)", D),
+        Week("Week(s)", WK),
+        Month("Month(s)", MO);
+
+        private final String units;
+        private final UnitsOfTimeEnum unitsOfTime;
+
+        UnitToDaysConverter(String units, UnitsOfTimeEnum unitsOfTime) {
+            this.units = units;
+            this.unitsOfTime = unitsOfTime;
+        }
+
+        public String getUnits() {
+            return units;
+        }
+
+        public UnitsOfTimeEnum getUnitsOfTime() {
+            return unitsOfTime;
+        }
     }
 
-//    private Map<String, UnitsOfTime> conceptNameToUnitMapper;
-//    private Map<String, UnitToDaysConverter> durationUnitToUnitMapper;
-//    private Map<UnitsOfTime, UnitToDaysConverter> unitsOfTimeMapper;
-//
-//    public UnitsHelpers() {
-//        buildConceptNameMapper();
-//        buildDurationUnitMapper();
-//        buildUnitsOfTimeToUnitsMapper();
-//    }
-//
-//    private void buildUnitsOfTimeToUnitsMapper() {
-//        unitsOfTimeMapper = new HashMap<>();
-//        unitsOfTimeMapper.put(h, UnitToDaysConverter.Hour);
-//        unitsOfTimeMapper.put(d, UnitToDaysConverter.Day);
-//        unitsOfTimeMapper.put(wk, UnitToDaysConverter.Week);
-//        unitsOfTimeMapper.put(mo, UnitToDaysConverter.Month);
-//    }
-//
-//    private void buildDurationUnitMapper() {
-//        durationUnitToUnitMapper = new HashMap<>();
-//        durationUnitToUnitMapper.put("Hour(s)", UnitToDaysConverter.Hour);
-//        durationUnitToUnitMapper.put("Day(s)", UnitToDaysConverter.Day);
-//        durationUnitToUnitMapper.put("Week(s)", UnitToDaysConverter.Week);
-//        durationUnitToUnitMapper.put("Month(s)", UnitToDaysConverter.Month);
-//    }
-//
-//    private void buildConceptNameMapper() {
-//        conceptNameToUnitMapper = new HashMap<>();
-//        conceptNameToUnitMapper.put("Once a day", d);
-//        conceptNameToUnitMapper.put("Twice a day", d);
-//        conceptNameToUnitMapper.put("Thrice a day", d);
-//        conceptNameToUnitMapper.put("Four times a day", d);
-//        conceptNameToUnitMapper.put("Every Hour", d);
-//        conceptNameToUnitMapper.put("Every 2 hours", d);
-//        conceptNameToUnitMapper.put("Every 3 hours", d);
-//        conceptNameToUnitMapper.put("Every 4 hours", d);
-//        conceptNameToUnitMapper.put("Every 6 hours", d);
-//        conceptNameToUnitMapper.put("Every 8 hours", d);
-//        conceptNameToUnitMapper.put("Every 12 hours", d);
-//        conceptNameToUnitMapper.put("On alternate days", d);
-//        conceptNameToUnitMapper.put("Once a week", wk);
-//        conceptNameToUnitMapper.put("Twice a week", wk);
-//        conceptNameToUnitMapper.put("Thrice a week", wk);
-//        conceptNameToUnitMapper.put("Every 2 weeks", wk);
-//        conceptNameToUnitMapper.put("Every 3 weeks", wk);
-//        conceptNameToUnitMapper.put("Once a month", mo);
-//    }
-//
-//    public Map<String, UnitToDaysConverter> getDurationUnitToUnitMapper() {
-//        return durationUnitToUnitMapper;
-//    }
-//
-//    public UnitsOfTime getUnitsOfTime(String conceptName) {
-//        return conceptNameToUnitMapper.get(conceptName);
-//    }
-//
-//    public Map<UnitsOfTime, UnitToDaysConverter> getUnitsOfTimeMapper() {
-//        return unitsOfTimeMapper;
-//    }
+    private Map<String, FrequencyUnits> conceptNameToFrequencyUnitMapper;
+    private Map<String, UnitToDaysConverter> durationUnitToUnitMapper;
+    private Map<UnitsOfTimeEnum, UnitToDaysConverter> unitsOfTimeMapper;
+
+    public UnitsHelpers() {
+        buildConceptNameMapper();
+        buildDurationUnitMapper();
+        buildUnitsOfTimeToUnitsMapper();
+    }
+
+    private void buildUnitsOfTimeToUnitsMapper() {
+        unitsOfTimeMapper = new HashMap<>();
+        unitsOfTimeMapper.put(H, UnitToDaysConverter.Hour);
+        unitsOfTimeMapper.put(D, UnitToDaysConverter.Day);
+        unitsOfTimeMapper.put(WK, UnitToDaysConverter.Week);
+        unitsOfTimeMapper.put(MO, UnitToDaysConverter.Month);
+    }
+
+    private void buildDurationUnitMapper() {
+        durationUnitToUnitMapper = new HashMap<>();
+        durationUnitToUnitMapper.put("Hour(s)", UnitToDaysConverter.Hour);
+        durationUnitToUnitMapper.put("Day(s)", UnitToDaysConverter.Day);
+        durationUnitToUnitMapper.put("Week(s)", UnitToDaysConverter.Week);
+        durationUnitToUnitMapper.put("Month(s)", UnitToDaysConverter.Month);
+    }
+
+    public enum FrequencyUnits {
+        ONCE_A_DAY("Once a day", 1, D, 1),
+        TWICE_A_DAY("Twice a day", 2, D, 1),
+        THRICE_A_DAY("Thrice a day", 3, D, 1),
+        FOUR_TIMES_A_DAY("Four times a day", 4, D, 1),
+        EVERY_HOUR("Every Hour", 1, H, 1),
+        EVERY_TWO_HOURS("Every 2 hours", 2, H, 1),
+        EVERY_THREE_HOURS("Every 3 hours", 3, H, 1),
+        EVERY_FOUR_HOURS("Every 4 hours", 4, H, 1),
+        EVERY_SIX_HOURS("Every 6 hours", 6, H, 1),
+        EVERY_EIGHT_HOURS("Every 8 hours", 8, H, 1),
+        EVERY_TWELVE_HOURS("Every 12 hours", 12, H, 1),
+        ON_ALTERNATE_DAYS("On alternate days", 1, D, 2),
+        ONCE_A_WEEK("Once a week", 1, WK, 1),
+        TWICE_A_WEEK("Twice a week", 2, WK, 1),
+        THRICE_A_WEEK("Thrice a week", 3, WK, 1),
+        EVERY_TWO_WEEKS("Every 2 weeks", 1, WK, 2),
+        EVERY_THREE_WEEKS("Every 3 weeks", 1, WK, 3),
+        ONCE_A_MONTH("Once a month", 1, MO, 1);
+
+        private final String conceptName;
+        private int frequency;
+        private final UnitsOfTimeEnum unitOfTime;
+        private int frequencyPeriod;
+
+        FrequencyUnits(String conceptName, int frequency, UnitsOfTimeEnum unitOfTime, int frequencyPeriod) {
+            this.conceptName = conceptName;
+            this.frequency = frequency;
+            this.unitOfTime = unitOfTime;
+            this.frequencyPeriod = frequencyPeriod;
+        }
+
+        public String getConceptName() {
+            return conceptName;
+        }
+
+        public int getFrequency() {
+            return frequency;
+        }
+
+        public UnitsOfTimeEnum getUnitOfTime() {
+            return unitOfTime;
+        }
+
+        public int getFrequencyPeriod() {
+            return frequencyPeriod;
+        }
+    }
+
+    private void buildConceptNameMapper() {
+        conceptNameToFrequencyUnitMapper = new HashMap<>();
+        conceptNameToFrequencyUnitMapper.put("Once a day", FrequencyUnits.ONCE_A_DAY);
+        conceptNameToFrequencyUnitMapper.put("Twice a day", FrequencyUnits.TWICE_A_DAY);
+        conceptNameToFrequencyUnitMapper.put("Thrice a day", FrequencyUnits.THRICE_A_DAY);
+        conceptNameToFrequencyUnitMapper.put("Four times a day", FrequencyUnits.FOUR_TIMES_A_DAY);
+        conceptNameToFrequencyUnitMapper.put("Every Hour", FrequencyUnits.EVERY_HOUR);
+        conceptNameToFrequencyUnitMapper.put("Every 2 hours", FrequencyUnits.EVERY_TWO_HOURS);
+        conceptNameToFrequencyUnitMapper.put("Every 3 hours", FrequencyUnits.EVERY_THREE_HOURS);
+        conceptNameToFrequencyUnitMapper.put("Every 4 hours", FrequencyUnits.EVERY_FOUR_HOURS);
+        conceptNameToFrequencyUnitMapper.put("Every 6 hours", FrequencyUnits.EVERY_SIX_HOURS);
+        conceptNameToFrequencyUnitMapper.put("Every 8 hours", FrequencyUnits.EVERY_EIGHT_HOURS);
+        conceptNameToFrequencyUnitMapper.put("Every 12 hours", FrequencyUnits.EVERY_TWELVE_HOURS);
+        conceptNameToFrequencyUnitMapper.put("On alternate days", FrequencyUnits.ON_ALTERNATE_DAYS);
+        conceptNameToFrequencyUnitMapper.put("Once a week", FrequencyUnits.ONCE_A_WEEK);
+        conceptNameToFrequencyUnitMapper.put("Twice a week", FrequencyUnits.TWICE_A_WEEK);
+        conceptNameToFrequencyUnitMapper.put("Thrice a week", FrequencyUnits.THRICE_A_WEEK);
+        conceptNameToFrequencyUnitMapper.put("Every 2 weeks", FrequencyUnits.EVERY_TWO_WEEKS);
+        conceptNameToFrequencyUnitMapper.put("Every 3 weeks", FrequencyUnits.EVERY_THREE_WEEKS);
+        conceptNameToFrequencyUnitMapper.put("Once a month", FrequencyUnits.ONCE_A_MONTH);
+    }
+
+    public Map<String, UnitToDaysConverter> getDurationUnitToUnitMapper() {
+        return durationUnitToUnitMapper;
+    }
+
+    public FrequencyUnits getFrequencyUnits(String conceptName) {
+        return conceptNameToFrequencyUnitMapper.get(conceptName);
+    }
+
+    public Map<UnitsOfTimeEnum, UnitToDaysConverter> getUnitsOfTimeMapper() {
+        return unitsOfTimeMapper;
+    }
 }
