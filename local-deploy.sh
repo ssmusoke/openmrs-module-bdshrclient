e9351979972ae6e3dee2789be2a3718a2382b1c9
Sanjana Bayya <sanjanb@thoughtworks.com>
2015-10-06 11:32:04 +0530

    Sanjana | BDSHR - 972 | upgraded omod version to 2.0

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mvn clean package -DskipTests
-scp -i ~/.ssh/id_rsa shrclient-omod/target/shrclient-1.0-SNAPSHOT.omod bahmni@192.168.33.18:~/.OpenMRS/modules
+scp -i ~/.ssh/id_rsa shrclient-omod/target/shrclient-2.0-SNAPSHOT.omod bahmni@192.168.33.18:~/.OpenMRS/modules
 scp -i ~/.ssh/id_rsa shrclient-omod/src/main/resources/*.properties bahmni@192.168.33.18:~/.OpenMRS/
