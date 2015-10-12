1657eeb242203c33b6f540cbc3a7489b9b0767ed
sukreetr <sukreetr@INsukreetr.local>
2015-10-12 14:30:28 +0530

    sukreet | BSHR-986 | Removing spaces from patient attributes

--- fhirmapper/src/main/java/org/openmrs/module/fhir/utils/Constants.java
+++ fhirmapper/src/main/java/org/openmrs/module/fhir/utils/Constants.java
@@ -5,23 +5,23 @@ public class Constants {
 
     public static final String SHR_CLIENT_SYSTEM_NAME = "shrclientsystem";
 
-    public static final String NATIONAL_ID_ATTRIBUTE = "National ID";
+    public static final String NATIONAL_ID_ATTRIBUTE = "nationalId";
 
-    public static final String HEALTH_ID_ATTRIBUTE = "Health ID";
+    public static final String HEALTH_ID_ATTRIBUTE = "healthId";
 
-    public static final String BIRTH_REG_NO_ATTRIBUTE = "Birth Registration Id";
+    public static final String BIRTH_REG_NO_ATTRIBUTE = "birthRegistrationId";
 
     public static final String GIVEN_NAME_LOCAL = "givenNameLocal";
 
     public static final String FAMILY_NAME_LOCAL = "familyNameLocal";
 
-    public static final String PHONE_NUMBER = "Phone Number";
+    public static final String PHONE_NUMBER = "phoneNumber";
 
     public static final String OCCUPATION_ATTRIBUTE = "occupation";
 
     public static final String EDUCATION_ATTRIBUTE = "education";
     
-    public static final String HOUSE_HOLD_CODE_ATTRIBUTE = "House Hold Code";
+    public static final String HOUSE_HOLD_CODE_ATTRIBUTE = "householdCode";
 
     public static final String IDENTIFIER_SOURCE_NAME = "BDH";
 
