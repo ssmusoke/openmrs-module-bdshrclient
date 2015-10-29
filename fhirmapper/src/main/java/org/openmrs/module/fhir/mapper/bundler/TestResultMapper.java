633235f950049678e893aefdfa0ebc0c47758a7d
sukreetr <sukreetr@thoughtworks.com>
2015-10-29 17:20:46 +0530

    Neha, sukreet | BSHR-1018 | Enabling upload of notes along with test results

--- fhirmapper/src/main/java/org/openmrs/module/fhir/mapper/bundler/TestResultMapper.java
+++ fhirmapper/src/main/java/org/openmrs/module/fhir/mapper/bundler/TestResultMapper.java
@@ -9,6 +9,7 @@ import ca.uhn.fhir.model.dstu2.resource.Observation;
 import ca.uhn.fhir.model.dstu2.valueset.ObservationStatusEnum;
 import ca.uhn.fhir.model.primitive.DateTimeDt;
 import org.openmrs.Obs;
+import org.openmrs.module.fhir.mapper.model.CompoundObservation;
 import org.openmrs.module.fhir.utils.CodeableConceptService;
 import org.openmrs.module.shrclient.dao.IdMappingsRepository;
 import org.openmrs.module.shrclient.model.IdMapping;
@@ -42,14 +43,14 @@ public class TestResultMapper implements EmrObsResourceHandler {
     }
 
     @Override
-    public List<FHIRResource> map(Obs obs, Encounter fhirEncounter, SystemProperties systemProperties) {
+    public List<FHIRResource> map(Obs topLevelObs, Encounter fhirEncounter, SystemProperties systemProperties) {
         List<FHIRResource> FHIRResourceList = new ArrayList<>();
-        if (obs != null) {
-            if (!isPanel(obs)) {
-                buildTestResult(obs, fhirEncounter, FHIRResourceList, systemProperties);
+        if (topLevelObs != null) {
+            if (!isPanel(topLevelObs)) {
+                buildTestResult(topLevelObs, fhirEncounter, FHIRResourceList, systemProperties);
             } else {
-                for (Obs observation : obs.getGroupMembers()) {
-                    buildTestResult(observation, fhirEncounter, FHIRResourceList, systemProperties);
+                for (Obs testObsGroup : topLevelObs.getGroupMembers()) {
+                    buildTestResult(testObsGroup, fhirEncounter, FHIRResourceList, systemProperties);
                 }
             }
         }
@@ -60,17 +61,48 @@ public class TestResultMapper implements EmrObsResourceHandler {
         return obs.getConcept().getConceptClass().getName().equals(MRS_CONCEPT_CLASS_LAB_SET);
     }
 
-    private void buildTestResult(Obs obs, Encounter fhirEncounter, List<FHIRResource> FHIRResourceList, SystemProperties systemProperties) {
-        for (Obs observation : obs.getGroupMembers()) {
-            DiagnosticReport diagnosticReport = build(observation, fhirEncounter, FHIRResourceList, systemProperties);
-            if (diagnosticReport != null) {
-                FHIRResource FHIRResource = new FHIRResource("Diagnostic Report", diagnosticReport.getIdentifier(), diagnosticReport);
-                FHIRResourceList.add(FHIRResource);
+    private void buildTestResult(Obs topLevelTestObs, Encounter fhirEncounter, List<FHIRResource> fhirResourceList, SystemProperties systemProperties) {
+        DiagnosticReport diagnosticReport = build(topLevelTestObs, fhirEncounter,  systemProperties);
+        if (diagnosticReport != null) {
+            for (Obs resultObsGroup : topLevelTestObs.getGroupMembers()) {
+                FHIRResource resultResource = getResultResource(resultObsGroup, fhirEncounter, systemProperties);
+                if(resultResource == null) return;
+                ResourceReferenceDt resourceReference = diagnosticReport.addResult();
+                resourceReference.setReference(resultResource.getIdentifier().getValue());
+                fhirResourceList.add(resultResource);
+
             }
+            FHIRResource FHIRResource = new FHIRResource("Diagnostic Report", diagnosticReport.getIdentifier(), diagnosticReport);
+            fhirResourceList.add(FHIRResource);
         }
     }
 
-    private DiagnosticReport build(Obs obs, Encounter fhirEncounter, List<FHIRResource> fHIRResourceList, SystemProperties systemProperties) {
+    private FHIRResource getResultResource(Obs resultObsGroup, Encounter fhirEncounter, SystemProperties systemProperties) {
+        CompoundObservation resultGroupObservation = new CompoundObservation(resultObsGroup);
+        FHIRResource observationResource = getResultObservation(resultObsGroup, fhirEncounter, systemProperties, resultGroupObservation);
+
+        Observation fhirObservation = (Observation) observationResource.getResource();
+        setObservationStatusForResult(fhirObservation);
+        setObservationNotes(resultGroupObservation, fhirObservation);
+
+        return observationResource;
+    }
+
+    private void setObservationNotes(CompoundObservation resultGroupObservation, Observation fhirObservation) {
+        Obs labNotesObs = resultGroupObservation.getMemberObsForConceptName(MRS_CONCEPT_NAME_LAB_NOTES);
+        fhirObservation.setComments(labNotesObs.getValueText());
+    }
+
+    private void setObservationStatusForResult(Observation resultObservation) {
+        resultObservation.setStatus(ObservationStatusEnum.FINAL);
+    }
+
+    private FHIRResource getResultObservation(Obs resultObsGroup, Encounter fhirEncounter, SystemProperties systemProperties, CompoundObservation resultGroupObservation) {
+        Obs resultObs = resultGroupObservation.getMemberObsForConcept(resultObsGroup.getConcept());
+        return observationMapper.mapObservation(resultObs, fhirEncounter, systemProperties);
+    }
+
+    private DiagnosticReport build(Obs obs, Encounter fhirEncounter, SystemProperties systemProperties) {
         DiagnosticReport report = diagnosticReportBuilder.build(obs, fhirEncounter, systemProperties);
         CodeableConceptDt name = codeableConceptService.addTRCoding(obs.getConcept(), idMappingsRepository);
         if (name.getCoding() != null && name.getCoding().isEmpty()) {
@@ -88,18 +120,6 @@ public class TestResultMapper implements EmrObsResourceHandler {
 
         report.addRequest().setReference(encounterIdMapping.getUri());
 
-        for (Obs member : obs.getGroupMembers()) {
-            if (member.getConcept().equals(obs.getConcept())) {
-                FHIRResource observationResource = observationMapper.mapObservation(member, fhirEncounter, systemProperties);
-                Observation fhirObservation = (Observation) observationResource.getResource();
-                fhirObservation.setStatus(ObservationStatusEnum.FINAL);
-                ResourceReferenceDt resourceReference = report.addResult();
-                resourceReference.setReference(observationResource.getIdentifier().getValue());
-                fHIRResourceList.add(observationResource);
-            } else if (MRS_CONCEPT_NAME_LAB_NOTES.equals(member.getConcept().getName().getName())) {
-                report.setConclusion(member.getValueText());
-            }
-        }
         return report;
     }
 
